f0e1d3e139af763ad817f12ac0d0d5f75b976b25
ant1krinz <72338323+ant1krinz@users.noreply.github.com>
2021-01-08 00:48:28 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,191 @@
+import pygame
+import sys
+import os
+
+clock = pygame.time.Clock()
+pygame.init()
+size = WIDTH, HEIGHT = 1050, 700
+screen = pygame.display.set_mode(size)
+
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+walls_group = pygame.sprite.Group()
+bushes_group = pygame.sprite.Group()
+
+FPS = 60
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def start_screen():
+    fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
+    screen.blit(fon, (0, 0))
+    text_coord = 50
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+start_screen()
+
+
+def load_level(filename):
+    filename = "data/" + filename
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+    max_width = max(map(len, level_map))
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
+tile_images = {
+    'wall': load_image('wall.png'),
+    'empty': load_image('grass.png'),
+    'bush': load_image('leaves.png')
+}
+
+player_image = load_image('main_tank.png')
+
+tile_width = tile_height = 50
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+        if tile_type == 'wall':
+            walls_group.add(self)
+        if tile_type == 'bush':
+            bushes_group.add(self)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.image = player_image
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+        self.distinction = "w"
+
+    def update(self, event):
+        move = None
+        try:
+            if event.key == pygame.K_w:
+                move = (0, -tile_width)
+                if self.distinction == "w":
+                    pass
+                elif self.distinction == "s":
+                    self.image = pygame.transform.rotate(self.image, 180)
+                elif self.distinction == "a":
+                    self.image = pygame.transform.rotate(self.image, 270)
+                elif self.distinction == "d":
+                    self.image = pygame.transform.rotate(self.image, 90)
+                self.distinction = "w"
+
+            elif event.key == pygame.K_s:
+                move = (0, tile_width)
+                if self.distinction == "w":
+                    self.image = pygame.transform.rotate(self.image, 180)
+                elif self.distinction == "s":
+                    pass
+                elif self.distinction == "a":
+                    self.image = pygame.transform.rotate(self.image, 90)
+                elif self.distinction == "d":
+                    self.image = pygame.transform.rotate(self.image, 270)
+                self.distinction = "s"
+
+            elif event.key == pygame.K_d:
+                move = (tile_width, 0)
+                if self.distinction == "w":
+                    self.image = pygame.transform.rotate(self.image, 270)
+                elif self.distinction == "s":
+                    self.image = pygame.transform.rotate(self.image, 90)
+                elif self.distinction == "a":
+                    self.image = pygame.transform.rotate(self.image, 180)
+                elif self.distinction == "d":
+                    pass
+                self.distinction = "d"
+
+            elif event.key == pygame.K_a:
+                move = (-tile_width, 0)
+                if self.distinction == "w":
+                    self.image = pygame.transform.rotate(self.image, 90)
+                elif self.distinction == "s":
+                    self.image = pygame.transform.rotate(self.image, 270)
+                elif self.distinction == "a":
+                    pass
+                elif self.distinction == "d":
+                    self.image = pygame.transform.rotate(self.image, 180)
+                self.distinction = "a"
+
+            self.rect = self.rect.move(move)
+            if pygame.sprite.spritecollideany(self, walls_group):
+                self.rect = self.rect.move(-move[0], -move[1])
+        except Exception:
+            print("Используйте wasd для управления марио")
+
+
+player = None
+
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                Tile('empty', x, y)
+            elif level[y][x] == '#':
+                Tile('wall', x, y)
+            elif level[y][x] == ',':
+                Tile('bush', x, y)
+            elif level[y][x] == '@':
+                Tile('empty', x, y)
+                new_player = Player(x, y)
+    return new_player, x, y
+
+
+player, level_x, level_y = generate_level(load_level("level1.txt"))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            terminate()
+
+        if event.type == pygame.KEYDOWN:
+            player_group.update(event)
+        screen.fill((0, 0, 0))
+        all_sprites.draw(screen)
+        player_group.draw(screen)
+        pygame.display.flip()
